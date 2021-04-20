76f30fbfa485ee4b0f047c350cb3b857ea735830
wally-wally <wallys0213@gmail.com>
2021-04-20 23:37:48 +0900

    add: App.vue에 Header 컴포넌트 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Header from 'components/Header/Header';
 import ListPage from './pages/ListPage';
 import FormPage from './pages/FormPage';
 import DetailPage from './pages/DetailPage';
@@ -8,6 +9,7 @@ import NotFoundPage from './pages/NotFoundPage';
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/">
           <Redirect to="/list" />
