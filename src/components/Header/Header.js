19c2695a3c94d6bb56201648d7d42db527df6255
wally-wally <wallys0213@gmail.com>
2021-04-21 23:06:07 +0900

    add: 코드 끝에 세미콜론 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Header.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
 
 const Header = () => {
   return (
@@ -8,7 +8,7 @@ const Header = () => {
       <Link to="/list">리스트</Link>
       <Link to="/form">작성페이지</Link>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
