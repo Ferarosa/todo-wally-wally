43449ad4ecb5f63ddcc3f7a496ba05dfbab7666f
wally-wally <wallys0213@gmail.com>
2021-04-25 12:28:02 +0900

    add: 할일 항목에서 상세 보기 페이지로 연결하는 링크 추가

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './TodoItem.scss';
 
 const TodoItem = ({ todo }) => {
   return (
     <li className="todo-list-item">
-      {todo.id}. {todo.title}
+      <div className="todo-list-item-title">
+       {todo.id}. {todo.title}
+      </div>
+      <Link className="todo-list-item-link" to={`/detail/${todo.id}`}>
+        상세보기
+      </Link>
     </li>
   );
 };
