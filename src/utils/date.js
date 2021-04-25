7fe960a9ae7e6fd2f05f33b263db255d6c71a0c2
wally-wally <wallys0213@gmail.com>
2021-04-26 01:52:09 +0900

    update: 날짜 계산 유틸 함수 수정

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,15 +1,32 @@
 const date = {
   formatedDate(date, isShowTime = false) {
-    const yyymmdd = date.slice(0, 10);
-    const hhmmss = date.slice(11, 19);
-
-    const dateValueElements = [yyymmdd];
-
-    if (isShowTime) {
-      dateValueElements.push(hhmmss);
+    if (!date) {
+      return '';
     }
 
-    return dateValueElements.join(' ');
+    const fillZero = (value) => value <= 9 ? `0${value}` : value;
+
+    const parsedDate = new Date(Date.parse(date));
+
+    const year = parsedDate.getFullYear();
+    const month = parsedDate.getMonth() + 1;
+    const day = parsedDate.getDate();
+
+    const yyyymmdd = `${year}-${fillZero(month)}-${fillZero(day)}`;
+
+    const hour = parsedDate.getHours();
+    const minutes = parsedDate.getMinutes();
+    const seconds = parsedDate.getSeconds();
+
+    const time = `${fillZero(hour)}:${fillZero(minutes)}:${fillZero(seconds)}`;
+
+    const dateElements = [yyyymmdd];
+
+    if (isShowTime) {
+      dateElements.push(time);
+    }
+
+    return dateElements.join(' ');
   }
 }
 
